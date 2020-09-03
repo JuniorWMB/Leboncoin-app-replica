59d75c2d787d7c6645cd873af4ea771699f89fea
Junior <juniormbaya243@gmail.com>
2020-09-03 22:03:56 +0200

    add new info in page offer

--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,9 +1,43 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import "./offer.css";
 function Offer() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsloading] = useState(true);
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        "https://leboncoin-api.herokuapp.com/offer/" + id
+      );
+      setData(res.data);
+      setIsloading(false);
+    };
+    fetchData();
+  }, [id]);
+  // console.log("offer info", data.picture.secure_url);
   return (
     <div>
-      <h1>offer pages</h1>
+      {isLoading === true ? (
+        <h1>your offer come </h1>
+      ) : (
+        <>
+          <div className="offer__container">
+            <img
+              className="offer__picrure"
+              src={data.picture.secure_url}
+              alt={data.title}
+            />
+
+            <div className="offer__textInfo">
+              <h3>{data.title}</h3>
+              <h3>{data.price}</h3>
+            </div>
+          </div>
+          <p>{data.description}</p>
+        </>
+      )}
     </div>
   );
 }
